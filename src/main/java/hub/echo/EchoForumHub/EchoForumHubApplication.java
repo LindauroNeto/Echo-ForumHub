1a8ce92d188e5fd68ea1756ef8499c6ca1b585ec
Lindauro Neto <netolindauro@gmail.com>
2025-01-07 20:40:44 -0300

    Iniciando o projeto

---
+++ src/main/java/hub/echo/EchoForumHub/EchoForumHubApplication.java
@@ -0,0 +1,13 @@
+package hub.echo.EchoForumHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoForumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hub/echo/EchoForumHub/EchoForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.echo.EchoForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
