63a975d6cbbed9df11da8fcee188d56d9e0b72ee
Lindauro Neto <netolindauro@gmail.com>
2025-01-18 00:20:41 -0300

    Configuração inicial

--- src/main/java/hub/forum/echo/domain/model/Usuario.java
+++ src/main/java/hub/forum/echo/domain/model/Usuario.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,8 +29,13 @@ public class Usuario implements UserDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, unique = true)
 	private Long id;
+	
+	@Column(nullable = false, unique = true)
 	private String usuario;
+	
+	@Column(nullable = false)
 	private String senha;
 
 	@Override
